6a722169d1217fccf52cb9167ca6fe14c52a71fe
ViktorCiganovic-collab <dcifw09@hotmail.se>
2024-11-23 23:13:38 +0100

    rounded borders footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='w-full h-20 flex justify-around my-5 text-white bg-slate-900 items-center'>
+    <footer className='w-full h-20 flex justify-around items-center my-5 text-white bg-slate-900 rounded-lg'>
         <p>Copyright</p>
         <p>About Us</p>
         <p>Contact</p>
